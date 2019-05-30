5b1166b269d22a69d697abecc5261a0e94e92b49
lvtu0316 <550407948@qq.com>
2019-05-30 17:41:01 +0800

    5

--- home_application/views.py
+++ home_application/views.py
@@ -54,9 +54,9 @@ def tables(request):
     return render(request, 'home_application/tables.html', locals())
 
 
-def get_capacity(request):
-    # user = User.objects.get(username='550407948')
-    client = get_client_by_request(request)  # 这里是周期任务，不能通过request请求client
+def get_capacity():
+    user = User.objects.get(username='550407948')
+    client = get_client_by_user(user.username)  # 这里是周期任务，不能通过request请求client
     fast_execute_script_result = fast_execute_script(client)
     # 如果快速脚本调用成功，执行log日志查询，获取执行内容
     if fast_execute_script_result['message'] == 'success':
